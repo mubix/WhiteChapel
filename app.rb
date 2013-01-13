9e228251a6407855f5231d7fe2da7fc5490c4469
Rob Fuller <jd.mubix@gmail.com>
2013-01-12 19:14:20 -0500

    its alive!!

--- app.rb
+++ app.rb
@@ -5,6 +5,11 @@ require 'haml'
 require 'tire'
 require 'rex'
 
+configure do
+  set :per_page, 25
+end
+
+
 helpers do
 	Tire.configure do
 		elasurl = File.open("elastic.conf").first
@@ -18,10 +23,27 @@ helpers do
 		store :title => 'Two',   :tags => ['ruby', 'python'], :published_on => '2011-01-02'
 		delete
 	end
+
+	Tire.index 'whitechapel-hashes' do
+		create
+		store :password => 'hello world', :hash => '5eb63bbbe01eeed093cb22bb8f5acdc3', :type => 'md5'
+		store :password => 'password', :hash => '5f4dcc3b5aa765d61d8327deb882cf99', :type => 'md5'
+	end
 end
 
 get '/' do
-	#haml :header
+
+	q = params[:q].to_s !~ /\S/ ? '*' : params[:q].to_s
+	f = params[:p].to_i*settings.per_page
+
+	@s = Tire.search( 'whitechapel-hashes' ) do |search|
+		search.query { |query| query.string q }
+		search.size settings.per_page
+		search.from f
+	end
+
+	# puts @s.to_curl
+
 	erb :search
 end
 
