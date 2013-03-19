e89ada6c56bf0027f6496a8daae4d6837796dc6d
Rob Fuller <jd.mubix@gmail.com>
2013-03-18 21:20:05 -0400

    actually import

--- scripts/dictionaryimport_cli.rb
+++ scripts/dictionaryimport_cli.rb
@@ -10,10 +10,14 @@ queue = []
 File.readlines("#{ARGV[0]}").each do |line|
 	queue << "#{line.chomp.force_encoding('UTF-8')}"
 	if counter == 10000 then
+		Resque.enqueue(EnqueueBulk, queue)
 		counter = 0
 		queue = []
 	end
 	counter += 1
 	count += 1
+end
+if queue.any?
+	Resque.enqueue(EnqueueBulk, queue)
 end
 puts "Imported #{count} lines"
