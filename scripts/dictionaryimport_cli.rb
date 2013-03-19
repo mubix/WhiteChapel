272e09395253df395ff23db5184e73786ded992d
Rob Fuller <jd.mubix@gmail.com>
2013-03-18 21:17:09 -0400

    make dictionary importer do 10k at a time

--- scripts/dictionaryimport_cli.rb
+++ scripts/dictionaryimport_cli.rb
@@ -5,8 +5,15 @@ require 'resque'
 require './importer'
 
 count = 0
+counter = 0
+queue = []
 File.readlines("#{ARGV[0]}").each do |line|
-	Resque.enqueue(EnqueuePasswords, line.chomp)
+	queue << "#{line.chomp.force_encoding('UTF-8')}"
+	if counter == 10000 then
+		counter = 0
+		queue = []
+	end
+	counter += 1
 	count += 1
 end
 puts "Imported #{count} lines"
